1530f5cea92328c474b05b24bd1986f96ee3f967
waduamani-coder <waduamani@gmail.com>
2026-02-26 11:22:28 +0300

    message

--- lesson1d.js
+++ lesson1d.js
@@ -2,12 +2,12 @@
 // A string is a series/sequence of characters that are enclosed inside of quotation marks.
 let name = "Amani Wadu"
 console.log("The name is: ", name)
-console.log(typeof(name))
+console.log(typeof (name))
 
 // floating point: This is a number with decimal
 let quantity = 35.5
 console.log("The quantity of sugar bought is: ", quantity)
-console.log(typeof(quantity))
+console.log(typeof (quantity))
 
 // Boolean : It returns true or false
 let isAdult = true
@@ -15,7 +15,7 @@ let isRegistered = false
 console.log("Is James an Adult? ", isAdult)
 console.log("Is he registered as a voter? ", isRegistered)
 
-console.log(typeof(isAdult))
+console.log(typeof (isAdult))
 
 
 // undefined: This is a data type that has been declared but therer is no value that has been assigned to it.
@@ -28,3 +28,28 @@ let username = null
 console.log("What is the username? ", username)
 
 //Assignment research and come up with examples on object and Array data types.Show how to access items of the object and of an array.
+
+
+//An object is a collection of key-value pairs.Each value is accessed using its key eg
+
+let student = {
+    name: "Alice",
+    age: 20,
+    course: "computer Science",
+    isEnrolled: true
+};
+console.log(student.name);
+console.log(student.age);
+
+console.log(student["course"]);
+console.log(student["isEnrolled"]);
+
+
+//An array is a collection of items stored in a specific order.Each item has an index number starting from 0
+
+let fruits = ["Apple", "Banana", "Mango", "Orange"];
+
+console.log(fruits[0]);
+console.log(fruits[2]);
+
+console.log(fruits[fruits.length - 1]);
